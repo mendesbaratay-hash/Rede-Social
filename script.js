dfa7a8699e413f323c2b43e18cf674e57ec399c6
mendesbaratay-hash <mendesbaratay@gmail.com>
2026-09-01 09:02:39 -0300

    ok

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = documet.querySelector (".post-media");
+if (!likeBtn) return;
+
+ const likesCountSpan = likeBtn.querySelector(".likes-count");
+ const bookmarkBtn = document.querySelector(".bookmark-btn");
+ 
+
+
+
+
+
+
+})
