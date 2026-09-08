d8a298e83cddcbc407fb4c1c923c262c37429e76
mendesbaratay-hash <mendesbaratay@gmail.com>
2026-09-08 08:58:15 -0300

    ok

--- script.js
+++ script.js
@@ -6,10 +6,32 @@ if (!likeBtn) return;
  const likesCountSpan = likeBtn.querySelector(".likes-count");
  const bookmarkBtn = document.querySelector(".bookmark-btn");
  
+let isLiked = false;
+let baseLikes = 0;
+
+if (likesCountSpan)
+    likesCountSpan.textContent = "0";
+}
+
+//números grandes 
+
+function formatLikes(num) {
+    if(num >=1000){
+        return (num/1000).toFixed(1)+"K";
+    }
+    return num.toString();
+}
+
+function addLike(){
+    baseLikes++;
+    isLiked = true
+    likeBtn.classList.add("liked");
+
+    if(likesCountSpan){
+        likesCountSpan.textContent = fortLikes(baseLikes);
+    }
+}
 
 
 
 
-
-
-})
